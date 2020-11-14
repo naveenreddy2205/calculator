81bed30ff9709e721b49f4b77ee88fb98ce5d9c5
unknown <naveenreddy.kampelli@edfenergy.com>
2020-11-14 13:07:21 +0530

    updated project

--- src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
+++ src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
@@ -1,26 +1,26 @@
-package com.ravi.cal.RaviCalculator;
+package com.ravi.cal.Calculator;
 
+import com.ravi.cal.RaviCalculator.Calculator;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+	
+	Calculator cal = new Calculator(2, 3);
+	
     @Test
     public void twoAndThreeIsFive() throws Exception {
-        final long result = new Calculator().addFucn(2, 3);
-        assertThat(result, is(5L));
+    	assertEquals(cal.addFucn(2, 3),5);
     }
 
     @Test
     public void threeMinusTwoIsOne() throws Exception {
-        final long result = new Calculator().subFucn(2, 3);
-        assertThat(result, is(1L));
+        assertEquals(cal.subFucn(2, 3),1);
     }
     
     @Test
     public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().mulFucn(3, 3);
-        assertThat(result, is(9L));
+        assertEquals(cal.mulFucn(3, 3), 9);
     }
    
 }
